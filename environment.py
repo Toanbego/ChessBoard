14bbf5bc8e190b9518d4f164458c730a8384095e
LAPTOP-AFNS1ELH\toanbego <torstein.gombos@gmail.com>
2018-12-29 14:11:45 +0100

    first commit

---
+++ environment.py
@@ -0,0 +1,39 @@
+"""
+Environment for chess board and pieces
+"""
+
+
+class ChessBoard:
+    """
+    Object for chess environment
+    """
+    def __init__(self):
+        """
+        Initialize attributes
+        """
+
+    def board(self):
+        """
+        Method for initializing the board
+
+        :return:
+        """
+
+    def chess_pieces(self):
+        """
+        Method for initializing the pieces
+        :return:
+        """
+
+    def rules(self):
+        """
+        Method that checks the rules for the various pieces
+        :return:
+        """
+
+    def move_piece(self, piece):
+        """
+        Method for moving pieces
+        :param piece:
+        :return:
+        """
